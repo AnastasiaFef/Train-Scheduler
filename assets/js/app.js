85cf89a9a3711ad5bc719ba77cd69f394cef5d76
AnastasiaFef <fefilovaanastasiya@gmail.com>
2017-12-22 00:57:20 -0800

    Perfect

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
 	var frequency;
 	var first;
 	var index=0;
-
+	var next;
+	var frequency_input;
+	var first_input;
 
 	//create config and initialize firebase
-	// Initialize Firebase
 	var config = {
 		apiKey: "AIzaSyCEB4quaq0Fr9TgdN0LYmyHTH7tKz8DFmg",
 		authDomain: "train-scheduler-be5f9.firebaseapp.com",
@@ -17,18 +18,15 @@ $(document).ready(function(){
 	};
 	firebase.initializeApp(config);
 
-	// var trains = firebase.database().ref("train/");
-
-
 	//Create a var to reference firebase
 	var database=firebase.database();
 
 	// (make sure can submit with Enter key) to .welcome
 	$(document).keyup(".start",function() {
-		  if (event.keyCode == 13) {
-		  	new_train();
-		  }
-		  else{return;}
+		if (event.keyCode == 13) {
+			new_train();
+		}
+		else{return;}
 	});
 
 	// on click #add_train_button
@@ -40,8 +38,10 @@ $(document).ready(function(){
 		var destination=$("#destination").val().trim();
 		var first_val=$("#first").val().trim();
 		first=moment(first_val, "HHmm").format("hh:mm A");
+		first_input=first;
 		var frequency_val=$("#frequency").val().trim();
 		frequency= moment(frequency_val, "mm").format("mm");
+		frequency_input=frequency;
 
 		console.log("firts "+first)
 
@@ -64,69 +64,57 @@ $(document).ready(function(){
 		});
 
 		index++;
-
-
-
 		console.log("New train");
 		$("#train-name").val('');
 		$("#destination").val('');
 		$("#first").val('');
 		$("#frequency").val('');
 	}
-	
-
-	// <tr>
-	// 	<td></td>
-	// 	<td></td>
-	// 	<td></td>
-	// 	<td></td>
-	// 	<td></td>
-	// </tr>
 
 	database.ref().on("child_added", function(data) {
-
       // Print the initial data to the console.
       console.log(data.val());
-
       // Log the value of the various properties
       console.log(data.val().name);
       console.log(data.val().destination);
       console.log(data.val().first_train);
       console.log(data.val().frequency);
 
-      when_next_train();
+      when_next_train(data);
 
       // Change the HTML
       var tr=$("<tr>");
       var td_name=$("<td>").append(data.val().name);
       var td_destination=$("<td>").append(data.val().destination);
-      var td_first=$("<td>").append(data.val().first_train);
-      var td_frequency=$("<td>").append(data.val().frequency);
-      var td_away=$("<td>").append("5");
+      var td_frequency=$("<td>").append(data.val().frequency+" min");
+      var td_next=$("<td>").append(next);
+      var td_away=$("<td>").append(away);
 
-      tr.append(td_name).append(td_destination).append(td_first).append(td_frequency).append(td_away)
+      tr.append(td_name).append(td_destination).append(td_frequency).append(td_next).append(td_away)
       $("#table").append(tr);
       // If any errors are experienced, log them to console.
     }, function(errorObject) {
       console.log("Error: " + errorObject.code);
     });
 
-    function when_next_train(){
-    	var currentTime = moment();
-		console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm A"));
-
-		//WORK ON THIS SHIT:
-
-		// first=moment(first, "hh:mm A").format("HH:mm");
-		// var diffTime = moment().diff(first);
-		// console.log("diffTime"+diffTime);
-
-		// away = diffTime % frequency;
-		// console.log("away "+away); 
-		// console.log("In when_next_train")
-
-
+    function when_next_train(data){
+    	if(!first_input){
+			first=data.val().first_train;
+		}
+		if(!frequency_input){
+			frequency=data.val().frequency;
+		}
+		var diffTime = moment().diff(moment(first, "hh:mm A"), 'minutes')*(-1);
+		away = diffTime % frequency;
+		// var currentTime = moment().format("HH:mm");
+		// console.log("currentTime "+currentTime)
+		// var away_formatted=moment(away, "minutes").format("HH:mm")
+		// console.log("away_formatted "+away_formatted)
+		// next=currentTime +away_formatted;
+		// console.log("next not formatted: "+next)
+		// next=moment(next).format("hh:mm A")
+		// console.log("alculated next formatted: "+next)
+		next=moment().add(away, "minutes").format("hh:mm A")
+		console.log("next "+next)
     }
-
-
 })
